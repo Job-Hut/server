3f09b7007035ae1ce7f1fb3f8ca16f404fe57bdf
indragusti <gustianto.indra18@gmail.com>
2024-10-30 15:22:07 +0700

    feat: add collection model, collection schema, and mutation create collection

---
+++ src/schema/collection.schema.ts
@@ -0,0 +1,113 @@
+import Collection from "../models/collection.model";
+
+export const typeDefs = `#graphql
+  type Task {
+    title: String
+    description: String
+    completed: Boolean
+    dueDate: String
+    createdAt: String
+    updatedAt: String
+  }
+
+  type Application {
+    ownerId: String
+    collectionId: String
+    jobTitle: String
+    organization: String
+    location: String
+    salary: Int
+    type: String
+    tasks: [Task]
+    startDate: String
+    endDate: String
+    createdAt: String
+    updatedAt: String
+  }
+
+  type Reply {
+    authorId: String
+    content: String
+    createdAt: String
+    updatedAt: String
+  }
+
+  type Thread {
+    title: String
+    content: String
+    authorId: String
+    replies: [Reply]
+    createdAt: String
+    updatedAt: String
+  }
+
+  type Chat {
+    senderId: String
+    content: String
+    createdAt: String
+    updatedAt: String
+  }
+
+  type Collection {
+    name: String
+    description: String
+    public: Boolean
+    ownerId: String
+    sharedWith: [String]
+    applications: [Application]
+    threads: [Thread]
+    chat: [Chat]
+    createdAt: String
+    updatedAt: String
+  }
+
+  type Query {
+    collections: [Collection]
+    collection(id: ID!): Collection
+  }
+
+  type Mutation {
+    createCollection(
+      name: String!
+      description: String!
+      public: Boolean!
+      ownerId: String!
+      sharedWith: [String]!
+      # applications: [Application]
+      # threads: [Thread]
+      # chat: [Chat]
+    ): Collection
+
+    deleteCollection(id: ID!): Collection
+  }
+`;
+
+export const resolvers = {
+  Mutation: {
+    createCollection: async (
+      _,
+      {
+        name,
+        description,
+        public: publicValue,
+        ownerId,
+        sharedWith,
+        // applications,
+        // threads,
+        // chat,
+      },
+    ) => {
+      const newCollection = new Collection({
+        name,
+        description,
+        public: publicValue,
+        ownerId,
+        sharedWith,
+        applications: [],
+        threads: [],
+        chat: [],
+      });
+      return await newCollection.save();
+    },
+  },
+};
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { ApolloServer } from "@apollo/server";
 import * as user from "./schema/user.schema";
 import * as book from "./schema/book.schema";
+import * as collection from "./schema/collection.schema";
 
 const server = new ApolloServer({
-  typeDefs: [book.typeDefs, user.typeDefs],
-  resolvers: [book.resolvers, user.resolvers],
+  typeDefs: [book.typeDefs, user.typeDefs, collection.typeDefs],
+  resolvers: [book.resolvers, user.resolvers, collection.resolvers],
 });
 
 export default server;
