ce62ed8172e502ad3c1bbe2de26714b2f69a9f7b
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-10-30 21:17:02 +0800

    feat: add jobs vacancy schema

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { ApolloServer } from "@apollo/server";
 import * as user from "./schema/user.schema";
 import * as book from "./schema/book.schema";
+import * as jobs from "./schema/jobs.schema";
 
 const server = new ApolloServer({
-  typeDefs: [book.typeDefs, user.typeDefs],
-  resolvers: [book.resolvers, user.resolvers],
+  typeDefs: [book.typeDefs, user.typeDefs, jobs.typeDefs],
+  resolvers: [book.resolvers, user.resolvers, jobs.resolvers],
 });
 
 export default server;
