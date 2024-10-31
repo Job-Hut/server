0653856dbfc059f2b05c427b51dc8c39c2c2b535
Muhammad Didi Nugroho <mdnugroho@students.hacktiv8.ac.id>
2024-10-31 15:43:26 +0700

    Revert "feat: added login feature"

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,54 +1,46 @@
 import mongoose from "mongoose";
-import { comparePassword, hashPassword } from "../helpers/bcrypt";
+import { hashPassword } from "../helpers/bcrypt";
 import { Profile, profileSchema } from "./profile.model";
 
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-    },
-    avatar: {
-      type: String,
-      required: false,
-    },
-    fullName: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    profile: {
-      type: profileSchema,
-    },
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
   },
-  { timestamps: true },
-);
+  avatar: {
+    type: String,
+    required: false,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profile: {
+    type: profileSchema
+  }
+}, { timestamps: true });;
 
 const User = mongoose.model("User", userSchema);
 
-type User = {
-  username: string;
-  avatar: string;
-  fullName: string;
-  email: string;
-  password: string;
-};
 
-export async function register(
-  username: string,
-  avatar: string,
-  fullName: string,
-  email: string,
-  password: string,
-) {
+type User = {
+  username: string
+  avatar: string
+  fullName: string
+  email: string
+  password: string
+}
+
+export async function register(username: string, avatar: string, fullName: string, email: string, password: string) {
   try {
     const isEmailValid = (email: string) => {
       const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -64,28 +56,28 @@ export async function register(
     }
 
     // todo default fallback image
-    if (!avatar) {
-      avatar = "";
+    if(!avatar) {
+      avatar = ""
     }
 
     if (password.length < 8) {
       throw new Error("Password must be at least 8 characters long.");
     }
 
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({email})
 
-    if (existingUser) {
-      throw new Error("Email already in use.");
+    if(existingUser) {
+      throw new Error("Email already in use.")
     }
-
-    const hashedPassword = hashPassword(password);
+    
+    const hashedPassword = hashPassword(password)
 
     const newUserProfile = new Profile({
       location: "",
       experiences: [],
       education: [],
-      licenses: [],
-    });
+      licenses: []
+    })
 
     const newUser = new User({
       username,
@@ -93,23 +85,15 @@ export async function register(
       fullName,
       email,
       password: hashedPassword,
-      profile: newUserProfile,
-    });
-
+      profile: newUserProfile
+    })
+    
     await newUser.save();
 
     return newUser;
   } catch (error) {
-    throw new Error(error.message);
+    throw new Error(error.message)
   }
-}
-
-export async function login(email: string, password: string) {
-  const user = await User.findOne({ email });
-  if (!user) throw new Error("Invalid Email/Password");
-  const isPasswordValid = comparePassword(password, user.password);
-  if (!isPasswordValid) throw new Error("Invalid Email/Password");
-  return user;
 }
 
 export default User;
