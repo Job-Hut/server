00d53792f339d1641689d1c61bfd02be524ec525
jalusw <jalusatrianiwijaya1@gmail.com>
2024-10-31 12:59:43 +0800

    fix: missing colon

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,8 @@ import * as jobs from "./schema/jobs.schema";
 import * as collection from "./schema/collection.schema";
 
 const server = new ApolloServer({
-  typeDefs: [book.typeDefs, user.typeDefs, collection.typeDefs,jobs.typeDefs]
-  resolvers: [book.resolvers, user.resolvers, collection.resolvers,jobs.resolvers]
+  typeDefs: [book.typeDefs, user.typeDefs, collection.typeDefs, jobs.typeDefs],
+  resolvers: [book.resolvers, user.resolvers, collection.resolvers, jobs.resolvers]
 });
 
 export default server;
