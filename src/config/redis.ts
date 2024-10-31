e0bcac279f064d929347d22487e2ea3c3d35e475
jalusw <jalusatrianiwijaya1@gmail.com>
2024-10-31 16:56:26 +0800

    chore: add redis connected log

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -4,6 +4,10 @@ import config from "./config";
 const redis = new Redis({
   host: config.redis.host,
   port: +config.redis.port,
+}).on("error", (error) => {
+  console.error(error);
+}).on("connect", () => {
+  console.log("Connected to Redis");
 });
 
 export default redis;
