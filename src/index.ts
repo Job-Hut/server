bf68c34fb0e2e4c0636b61f47e344bf2f0b8fa9d
jalusw <jalusatrianiwijaya1@gmail.com>
2024-11-01 21:01:12 +0800

    feat: add web socket server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,64 @@
-import { startStandaloneServer } from "@apollo/server/standalone";
-import server from "./server";
 import config from "./config/config";
 import mongoose from "mongoose";
+
+import { ApolloServer } from "@apollo/server";
+import * as user from "./schema/user.schema";
+import * as jobs from "./schema/jobs.schema";
+import * as collection from "./schema/collection.schema";
+import * as application from "./schema/application.schema";
+
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { createServer } from "http";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { WebSocketServer } from "ws";
+import { useServer } from "graphql-ws/lib/use/ws";
+import express from "express";
+import cors from "cors";
 import { verifyToken } from "./helpers/jwt";
 import User from "./models/user.model";
+
+const app = express();
+const httpServer = createServer(app);
+
+const schema = makeExecutableSchema({
+  typeDefs: [
+    user.typeDefs,
+    collection.typeDefs,
+    jobs.typeDefs,
+    application.typeDefs,
+  ],
+  resolvers: [
+    user.resolvers,
+    collection.resolvers,
+    jobs.resolvers,
+    application.resolvers,
+  ],
+});
+
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: "/subscriptions",
+});
+
+const serverCleanup = useServer({ schema }, wsServer);
+
+export const server = new ApolloServer({
+  schema,
+  introspection: true,
+  plugins: [
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+    {
+      async serverWillStart() {
+        return {
+          async drainServer() {
+            await serverCleanup.dispose();
+          },
+        };
+      },
+    },
+  ],
+});
 
 (async () => {
   mongoose
@@ -16,21 +71,29 @@ import User from "./models/user.model";
     .catch((err) => {
       console.error(err);
     });
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: +config.app.port },
-    context: async ({ req }) => {
-      return {
-        authentication: async () => {
-          const authorization = req.headers.authorization;
-          if(!authorization) throw new Error("You have to login first!")
-          const token = authorization.split(" ")[1]
-          if (!token) throw new Error("Invalid Token");
-          const decoded = verifyToken(token)
-          const user = await User.findById(decoded._id)
-          return user;
-        }
-      }
-    }
+  await server.start();
+
+  app.use(
+    "/graphql",
+    cors<cors.CorsRequest>(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        return {
+          authentication: async () => {
+            const authorization = req.headers.authorization;
+            if (!authorization) throw new Error("You have to login first!");
+            const token = authorization.split(" ")[1];
+            if (!token) throw new Error("Invalid Token");
+            const decoded = verifyToken(token);
+            const user = await User.findById(decoded._id);
+            return user;
+          },
+        };
+      },
+    }),
+  );
+  httpServer.listen(config.app.port, () => {
+    console.log(`Server is running on port ${config.app.port}`);
   });
-  console.log(`🚀 Server ready at: ${url}`);
 })();
