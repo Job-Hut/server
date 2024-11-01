ba1d98c500b72ca2f80343be5a6b160a2bd2fd21
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-11-01 15:12:03 +0800

    refactor: move secret token to env

--- src/helpers/jwt.ts
+++ src/helpers/jwt.ts
@@ -1,6 +1,5 @@
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
+import config from "../config/config";
 
 type Payload = {
   _id: string;
@@ -9,10 +8,10 @@ type Payload = {
 };
 
 export const signToken = (payload: Payload) => {
-  return jwt.sign(payload, process.env.JWT_SECRET);
+  return jwt.sign(payload, config.app.secret);
 };
 
 export const verifyToken = (token: string) => {
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  return decoded as Payload
+  const decoded = jwt.verify(token, config.app.secret);
+  return decoded as Payload;
 };
