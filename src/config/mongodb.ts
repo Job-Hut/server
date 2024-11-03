b02b0e161a16ac98b789b7b4f5d9f94364b08051
jalusw <jalusatrianiwijaya1@gmail.com>
2024-11-03 20:01:30 +0800

    test: add application test cases

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -2,13 +2,8 @@ import config from "./config";
 import mongoose from "mongoose";
 
 export const init = async () => {
-  try {
-    await mongoose.connect(config.mongodb.connectionString, {
-      dbName: config.mongodb.database,
-    });
-    console.log("Connected to mongodb");
-  } catch (error) {
-    console.log("Can't connect to mongodb");
-    console.error(error);
-  }
+  await mongoose.connect(config.mongodb.connectionString, {
+    dbName: config.mongodb.database,
+  });
+  console.log("Connected to mongodb");
 };
